5fc9735ec2f788fbea660cfe415c854d8175c200
Vincent Gerard <vincentpgerard@gmail.com>
2023-11-16 17:48:39 -0600

    update to .NET 8

--- RemoteShutdown/Program.cs
+++ RemoteShutdown/Program.cs
@@ -1,6 +1,14 @@
 using shutdownApi.Services;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var vaultUri = Environment.GetEnvironmentVariable("VaultUri");
+if (vaultUri is not null)
+{
+    var keyVaultEndpoint = new Uri(vaultUri);
+    builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+}
 
 // Add services to the container.
 builder.Services.AddControllers();
